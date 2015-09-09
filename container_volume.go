c0140e12484f63fe176db883140c4a3587226737
Ethan Mosbaugh <emosbaugh@gmail.com>
2015-09-09 16:28:39 -0700

    is volume included in backup

--- container_volume.go
+++ container_volume.go
@@ -1,8 +1,9 @@
 package libyaml
 
 type ContainerVolume struct {
-	HostPath      string `yaml:"host_path" json:"host_path"`
-	ContainerPath string `yaml:"container_path" json:"container_path"`
-	Permission    string `yaml:"permission" json:"permission"`
-	Owner         string `yaml:"owner" json:"owner"`
+	HostPath             string `yaml:"host_path" json:"host_path"`
+	ContainerPath        string `yaml:"container_path" json:"container_path"`
+	Permission           string `yaml:"permission" json:"permission"`
+	Owner                string `yaml:"owner" json:"owner"`
+	IsExcludedFromBackup string `yaml:"is_excluded_from_backup" json:"is_excluded_from_backup"`
 }
