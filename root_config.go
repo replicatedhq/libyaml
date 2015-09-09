2302070531cd2343037bcfeea2581a39357bac32
Ethan Mosbaugh <emosbaugh@gmail.com>
2015-09-09 16:28:19 -0700

    console support page markdown content

--- root_config.go
+++ root_config.go
@@ -1,19 +1,20 @@
 package libyaml
 
 type RootConfig struct {
-	ApiVersion     string           `yaml:"replicated_api_version" json:"replicated_api_version"`
-	Name           string           `yaml:"name" json:"name"`
-	Version        string           `yaml:"version" json:"version"`
-	ReleaseNotes   string           `yaml:"release_notes" json:"release_notes"`
-	Properties     Properties       `yaml:"properties" json:"properties"`
-	Identity       Identity         `yaml:"identity" json:"identity"`
-	State          State            `yaml:"state" json:"state"`
-	Backup         Backup           `yaml:"backup" json:"backup"`
-	Monitors       Monitors         `yaml:"monitors" json:"monitors"`
-	Components     []*Component     `yaml:"components" json:"components"`
-	ConfigCommands []*ConfigCommand `yaml:"cmds" json:"cmds"`
-	ConfigGroups   []*ConfigGroup   `yaml:"config" json:"config"`
-	AdminCommands  []*AdminCommand  `yaml:"admin_commands" json:"admin_commands"`
+	ApiVersion             string           `yaml:"replicated_api_version" json:"replicated_api_version"`
+	Name                   string           `yaml:"name" json:"name"`
+	Version                string           `yaml:"version" json:"version"`
+	ReleaseNotes           string           `yaml:"release_notes" json:"release_notes"`
+	ConsoleSupportMarkdown string           `yaml:"console_support_markdown" json:"console_support_markdown"`
+	Properties             Properties       `yaml:"properties" json:"properties"`
+	Identity               Identity         `yaml:"identity" json:"identity"`
+	State                  State            `yaml:"state" json:"state"`
+	Backup                 Backup           `yaml:"backup" json:"backup"`
+	Monitors               Monitors         `yaml:"monitors" json:"monitors"`
+	Components             []*Component     `yaml:"components" json:"components"`
+	ConfigCommands         []*ConfigCommand `yaml:"cmds" json:"cmds"`
+	ConfigGroups           []*ConfigGroup   `yaml:"config" json:"config"`
+	AdminCommands          []*AdminCommand  `yaml:"admin_commands" json:"admin_commands"`
 }
 
 const DEFAULT_APP_CONFIG = `
@@ -23,10 +24,5 @@ name: "%s"
 version: ""
 release_notes: ""
 components: []
-backup:
-  enabled: false
-  init: []
-  run: []
-cmds: []
 config: []
 `
