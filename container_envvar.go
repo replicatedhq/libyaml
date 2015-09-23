e677d74afb6c3909d049f795ebb972025b6f3a8a
Yeghishe Piruzyan <ypiruzyan@gmail.com>
2015-09-22 21:25:50 -0700

    adding when to customerfile and envvar

--- container_envvar.go
+++ container_envvar.go
@@ -4,4 +4,5 @@ type ContainerEnvVar struct {
 	Name                  string `yaml:"name" json:"name"`
 	StaticVal             string `yaml:"static_val" json:"static_val"`
 	IsExcludedFromSupport string `yaml:"is_excluded_from_support" json:"is_excluded_from_support"`
+	When                  string `yaml:"when" json:"when"`
 }
