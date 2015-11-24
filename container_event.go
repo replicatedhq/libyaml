136cd1fee862abb09e9960007accd8dd6b7e34fe
Marc Campbell <marc.e.campbell@gmail.com>
2015-11-23 20:39:33 -0800

    args for exec command

--- container_event.go
+++ container_event.go
@@ -4,5 +4,6 @@ type ContainerEvent struct {
 	Name          string                        `yaml:"name" json:"name"`
 	Trigger       string                        `yaml:"trigger" json:"trigger"`
 	Data          string                        `yaml:"data" json:"data"`
+	Args          []string                      `yaml:"args" json:"args"`
 	Subscriptions []*ContainerEventSubscription `yaml:"subscriptions" json:"subscriptions" validate:"dive"`
 }
