d3b076c07e14442fd22a3661b48a475a4f31588c
Joe Toscano <joe@replicated.com>
2015-12-02 15:51:31 -0800

    Fix validation func name

--- container_supportcommand.go
+++ container_supportcommand.go
@@ -1,6 +1,6 @@
 package libyaml
 
 type ContainerSupportCommand struct {
-	Filename string   `yaml:"filename" json:"filename" validate:"nonzero"`
+	Filename string   `yaml:"filename" json:"filename" validate:"required"`
 	Command  []string `yaml:"command" json:"command"`
 }
