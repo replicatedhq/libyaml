645bfd8d035b71bce8819046d3e0acf298f0a4d1
divolgin <dmitriy@replicated.com>
2016-02-03 10:03:18 -0800

    REPLICATED-1174: Allow templates in pause_all flag for backups

--- backup.go
+++ backup.go
@@ -1,7 +1,8 @@
 package libyaml
 
 type Backup struct {
-	Enabled  string `yaml:"enabled" json:"enabled"`
-	PauseAll bool   `yaml:"pause_all" json:"pause_all"`
-	Script   string `yaml:"script" json:"script"`
+	Enabled         string `yaml:"enabled" json:"enabled"`
+	PauseAll        bool   `yaml:"pause_all" json:"pause_all"` // deprecated
+	PauseContainers string `yaml:"pause_containers" json:"pause_containers"`
+	Script          string `yaml:"script" json:"script"`
 }
