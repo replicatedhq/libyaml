1f9846c83f6783c22233c02d48821f730279a44d
bueller <bueller@gmail.com>
2016-04-06 17:03:34 -0400

    empty pointers should not be iterated over in custom exist validations

--- config_item.go
+++ config_item.go
@@ -22,7 +22,7 @@ type ConfigItem struct {
 	Required              bool                   `yaml:"required" json:"required"`
 	TestProc              *TestProc              `yaml:"test_proc" json:"test_proc"`
 	IsExcludedFromSupport bool                   `yaml:"is_excluded_from_support" json:"is_excluded_from_support"`
-	Items                 []*ConfigItem          `yaml:"items" json:"items"`
+	Items                 []*ConfigItem          `yaml:"items" json:"items" validate:"dive,exists"`
 }
 
 type ConfigItemCmd struct {
