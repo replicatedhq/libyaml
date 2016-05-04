254e8e8e82790e9d8b0eb9f99ca0a6e69c363aab
Ethan Mosbaugh <emosbaugh@gmail.com>
2016-05-04 15:33:08 -0700

    [REPLICATED-1340] cpu mhz

--- host_requirements.go
+++ host_requirements.go
@@ -3,12 +3,14 @@ package libyaml
 type HostRequirements struct {
 	DockerVersion string `yaml:"docker_version" json:"docker_version" validate:"semver"`
 	CPUCores      uint   `yaml:"cpu_cores" json:"cpu_cores"`
+	CPUMhz        uint   `yaml:"cpu_mhz" json:"cpu_mhz"`
 	Memory        string `yaml:"memory" json:"memory" validate:"bytes"`
 	DiskSpace     string `yaml:"disk_space" json:"disk_space" validate:"bytes"`
 }
 
 type ComponentHostRequirements struct {
 	CPUCores  uint   `yaml:"cpu_cores" json:"cpu_cores"`
+	CPUMhz    uint   `yaml:"cpu_mhz" json:"cpu_mhz"`
 	Memory    string `yaml:"memory" json:"memory" validate:"bytes"`
 	DiskSpace string `yaml:"disk_space" json:"disk_space" validate:"bytes"`
 }
