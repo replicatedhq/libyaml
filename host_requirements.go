c315c2a7e7e60a4abe488d458802944e3f33225a
divolgin <dmitriy@replicated.com>
2016-10-27 11:13:28 -0700

    [REPLICATED-1758] Automatically pull best replicated version

--- host_requirements.go
+++ host_requirements.go
@@ -1,11 +1,12 @@
 package libyaml
 
 type HostRequirements struct {
-	DockerVersion string `yaml:"docker_version" json:"docker_version" validate:"semver"`
-	CPUCores      uint   `yaml:"cpu_cores" json:"cpu_cores"`
-	CPUMhz        uint   `yaml:"cpu_mhz" json:"cpu_mhz"`
-	Memory        string `yaml:"memory" json:"memory" validate:"bytes"`
-	DiskSpace     string `yaml:"disk_space" json:"disk_space" validate:"bytes"`
+	DockerVersion     string `yaml:"docker_version" json:"docker_version" validate:"semver"`
+	ReplicatedVersion string `yaml:"replicated_version" json:"version" validate:"semverrange"`
+	CPUCores          uint   `yaml:"cpu_cores" json:"cpu_cores"`
+	CPUMhz            uint   `yaml:"cpu_mhz" json:"cpu_mhz"`
+	Memory            string `yaml:"memory" json:"memory" validate:"bytes"`
+	DiskSpace         string `yaml:"disk_space" json:"disk_space" validate:"bytes"`
 }
 
 type ComponentHostRequirements struct {
