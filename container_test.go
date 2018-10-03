7c30172c3eff5fd6db89293c57d31d90ebb430fd
Robert Oh <robert.is.oh@gmail.com>
2018-10-03 12:44:46 -0700

    add stop_timeout to container

--- container_test.go
+++ container_test.go
@@ -152,6 +152,7 @@ dynamic: ""
 pid_mode: ""
 shm_size: 0
 labels: []
+stop_timeout: 0
 `
 
 	logReqs := libyaml.LogOptions{
@@ -217,6 +218,7 @@ dynamic: ""
 pid_mode: ""
 shm_size: 0
 labels: []
+stop_timeout: 0
 `
 
 	c := libyaml.Container{
