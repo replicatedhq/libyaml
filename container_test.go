e62b9c4fdc750311e2585f77bbc5ae3da0d5389c
Robert Oh <robert.is.oh@gmail.com>
2018-10-03 17:06:44 -0700

    string

--- container_test.go
+++ container_test.go
@@ -152,7 +152,7 @@ dynamic: ""
 pid_mode: ""
 shm_size: 0
 labels: []
-stop_timeout: 0
+stop_timeout: ""
 `
 
 	logReqs := libyaml.LogOptions{
@@ -218,7 +218,7 @@ dynamic: ""
 pid_mode: ""
 shm_size: 0
 labels: []
-stop_timeout: 0
+stop_timeout: ""
 `
 
 	c := libyaml.Container{
