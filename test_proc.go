635657252d5408e193b1ca28e5d71c4a6fbdf2f2
emosbaugh <emosbaugh@gmail.com>
2018-10-26 10:08:12 -0700

    Omitempty test proc custom command (#135)

--- test_proc.go
+++ test_proc.go
@@ -3,7 +3,7 @@ package libyaml
 type TestProc struct {
 	DisplayName   string                `yaml:"display_name" json:"display_name"`
 	Command       string                `yaml:"command" json:"command"`
-	CustomCommand CustomCommand         `yaml:"custom_command" json:"custom_command"`
+	CustomCommand *CustomCommand        `yaml:"custom_command,omitempty" json:"custom_command,omitempty" validate:"omitempty"`
 	Results       []CustomCommandResult `yaml:"results" json:"results" validate:"dive"`
 	Timeout       uint                  `yaml:"timeout" json:"timeout"`
 	ArgFields     []string              `yaml:"arg_fields" json:"arg_fields"`
