a2e552a73ca15f755ca3a857aaa2ccb1dc1a62b5
Ethan Mosbaugh <ethan@replicated.com>
2020-05-05 06:19:02 -0700

    ports[].public_port_initial [ch24367]

--- container_port.go
+++ container_port.go
@@ -1,9 +1,10 @@
 package libyaml
 
 type ContainerPort struct {
-	PrivatePort string `yaml:"private_port" json:"private_port" validate:"required"`
-	PublicPort  string `yaml:"public_port,omitempty" json:"public_port,omitempty" validate:"required_minapiversion=2.8.0"`
-	Interface   string `yaml:"interface,omitempty" json:"interface,omitempty"`
-	PortType    string `yaml:"port_type,omitempty" json:"port_type,omitempty"`
-	When        string `yaml:"when,omitempty" json:"when,omitempty"`
+	PrivatePort       string `yaml:"private_port" json:"private_port" validate:"required"`
+	PublicPort        string `yaml:"public_port,omitempty" json:"public_port,omitempty" validate:"required_minapiversion=2.8.0"`
+	PublicPortInitial string `yaml:"public_port_initial,omitempty" json:"public_port_initial,omitempty" validate:"required_minapiversion=2.8.0"`
+	Interface         string `yaml:"interface,omitempty" json:"interface,omitempty"`
+	PortType          string `yaml:"port_type,omitempty" json:"port_type,omitempty"`
+	When              string `yaml:"when,omitempty" json:"when,omitempty"`
 }
